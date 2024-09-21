9dac119e72374fddb9c172e3a98c0386e933956d
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2024-09-21 19:40:44 +0600

    adding used some middlewares

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import app from "./app.js";
 import colors from "colors";
 import dev from "./config/config.js";
 const PORT = dev.port;
+// note: server listen
 app.listen(PORT, () => {
   console.log(
     colors.bgGreen.black(`Server is running on http://localhost:${PORT}`)
