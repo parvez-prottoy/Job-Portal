a2e8992bf7c26acf304164bfd8bd1ad7f1c76de8
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2024-09-21 20:54:34 +0600

    adding error handler

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import { notFoundHandler, errorHandler } from "./src/utils/error.js";
 
 const app = express();
 // note: connect mongoDB
@@ -13,5 +14,8 @@ app.use([
   express.json(),
   express.urlencoded({ extended: true }),
 ]);
+// note: error handler
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 export default app;
