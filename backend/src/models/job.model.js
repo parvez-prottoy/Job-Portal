dc1cb7e1fe9c437e0125adf53af1d633a8a89b17
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2024-09-22 19:19:40 +0600

    adding job model timestamps and versionkey

--- backend/src/models/job.model.js
+++ backend/src/models/job.model.js
@@ -1,61 +1,64 @@
 import mongoose from "mongoose";
 
-const jobSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    require: true,
-  },
-  description: {
-    type: String,
-    require: true,
-  },
-  jobLocation: {
-    type: String,
-    require: true,
-  },
-  salary: {
-    type: Number,
-    require: true,
-  },
-  type: {
-    type: String,
-    require: true,
-  },
-  position: {
-    type: Number,
-    require: true,
-  },
-  responsibilities: [
-    {
+const jobSchema = new mongoose.Schema(
+  {
+    title: {
       type: String,
+      require: true,
     },
-  ],
-  skillsExperience: [
-    {
+    description: {
       type: String,
+      require: true,
     },
-  ],
-  skills: [
-    {
+    jobLocation: {
       type: String,
+      require: true,
     },
-  ],
-  company: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "companies",
-    require: true,
-  },
-  createBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "users",
-    require: true,
-  },
-  applications: [
-    {
+    salary: {
+      type: Number,
+      require: true,
+    },
+    type: {
+      type: String,
+      require: true,
+    },
+    position: {
+      type: Number,
+      require: true,
+    },
+    responsibilities: [
+      {
+        type: String,
+      },
+    ],
+    skillsExperience: [
+      {
+        type: String,
+      },
+    ],
+    skills: [
+      {
+        type: String,
+      },
+    ],
+    company: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "applications",
+      ref: "companies",
+      require: true,
     },
-  ],
-});
+    createBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      require: true,
+    },
+    applications: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "applications",
+      },
+    ],
+  },
+  { timestamps: true, versionKey: false }
+);
 const JobModel = mongoose.model("jobs", jobSchema);
 export default JobModel;
